649abdc74a22fdb755b1b0c8a47be410c0378156
naghamalrjoub <nagham.rjoub@gmail.com>
2026-02-27 21:24:36 +0300

    checkout addition

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ async function fetchBooks() {
         cardsRow.append(col);
         const card = document.createElement("div");
         card.className = "card";
-        card.id = "card";
         card.style = "width: 18rem"
         col.append(card);
 
@@ -78,14 +77,96 @@ homeButton.addEventListener("click", viewHomePage);
 
 
 const emptyCart = document.querySelector("#emptyCart");
-let totalBooks = 0;
+const notEmptyCart = document.querySelector("#notEmptyCart")
+let totalBooks = 0, priceForPayment = 0;
+let cartArray = [];
 const addToCart = (book) => {
-    if (!emptyCart.classList.contains("d-none"))
+    if (!emptyCart.classList.contains("d-none")) {
         emptyCart.classList.add("d-none");
+        notEmptyCart.classList.remove("d-none");
+    }
+        
     totalBooks++;
     console.log(totalBooks);
+    let findBook = cartArray.find(item => item === book.isbn13);
+    if (findBook) {
 
-    
+    }
+
+    else {
+        cartArray.push(book.isbn13);
+        appendBook(book);
+    }
+}
+
+const appendBook = (book) => {
+
+    const booksInCart = document.querySelector("#booksInCart");
+    //create card and append it
+    const card = document.createElement("div");
+    card.className = "card mt-3 container-fluid";
+    booksInCart.append(card)
+
+    const row = document.createElement("row");
+    row.className = "row g-0";
+    card.append(row);
+
+    //create image div
+    const imageDiv = document.createElement("div");
+    imageDiv.className = "col-md-4";
+    row.append(imageDiv);
+    const img = document.createElement("img")
+    img.src = book.image;
+    img.className = "img-fluid rounded-start";
+    imageDiv.append(img);
+
+    //card body div
+    const bodyDiv = document.createElement("div");
+    bodyDiv.className = "col-md-8 d-flex";
+    row.append(bodyDiv);
+
+    //card body
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    cardBody.id = "cartCardBody";
+    bodyDiv.append(cardBody);
+
+    //body content;
+    //get title
+    const title = document.createElement("h5");
+    title.className = "card-title"
+    title.innerText = book.title;
+    cardBody.append(title);
+    //get subtitle
+    const subtitle = document.createElement("p");
+    subtitle.className = "card-text";
+    subtitle.innerText = book.subtitle;
+    cardBody.append(subtitle);
+    //get price
+    const price = document.createElement("p");
+    price.className = "card-text";
+    price.innerText = book.price;
+    cardBody.append(price);
+    //footing div for each card
+    const cardFooter = document.createElement("div");
+    cardFooter.id = "cardFooter";
+    cardBody.append(cardFooter);
+    //append button
+    const removeButton = document.createElement("a");
+    removeButton.href = "#";
+    removeButton.className = "btn btn-outline-danger";
+    removeButton.id = "removeFromCartBtn"
+    removeButton.innerText = "remove from cart";
+    cardFooter.append(removeButton);
+    let actualPrice = book.price.slice(1);
+    priceForPayment += parseFloat(actualPrice);
+    console.log(priceForPayment);
+
+    //edit the total price & total items section
+    const totalPriceSpan = document.querySelector("#totalPriceSpan");
+    const totalItemsSpan = document.querySelector("#totalItemsSpan")
+    totalPriceSpan.innerText = '$' + priceForPayment;
+    totalItemsSpan.innerText = totalBooks; 
 }
 
 const removeFromCart = () => {
